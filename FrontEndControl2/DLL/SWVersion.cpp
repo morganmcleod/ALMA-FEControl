7f5549dce8ba5f23ac6d08b7e06ce8caacda0f47
Morgan McLeod <mmcleod@nrao.edu>
2018-02-28 13:01:20 -0300

    DLL 2.8.7-beta4

--- FrontEndControl2/DLL/SWVersion.cpp
+++ FrontEndControl2/DLL/SWVersion.cpp
@@ -21,7 +21,7 @@
 
 #include "SWVersion.h"
 
-const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.8.7-beta3");
+const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.8.7-beta4");
 
 //******* Be sure to update resource.rc!
 
