9eb8c9ea8052f68b7bfd3bcecdf762e24eb93f91
Morgan McLeod <mmcleod@nrao.edu>
2019-04-23 16:45:40 -0400

    FrontEndControl 2.10.1 Now requires libexpatmm-0.dll

--- FrontEndControl2/DLL/SWVersion.cpp
+++ FrontEndControl2/DLL/SWVersion.cpp
@@ -21,7 +21,7 @@
 
 #include "SWVersion.h"
 
-const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.10.0");
+const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.10.1");
 
 //******* Be sure to update resource.rc!
 
