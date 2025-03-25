morganmcleod/ALMA-FEControl

commit dbe2c97de00c0a242aa2ec36bece67c88537506a

Measire NICanBusInterface performance in us Pause cartridge moniutoring for 500ms pefore band power-off (to prevent crash, hopefully.) Added EVENT_ALL_REPS_DONE for IV curve and optimize measurements

FrontEndControl2/OPTIMIZE/MeasureIFPower.cpp
@@ -362,6 +362,8 @@ void MeasureIFPower::optimizeAction() {
             SLEEP(stepSleep);
             waitForPowerReading_m = true;
         }
+        // Send the measurement finished event:
+        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
         
     // All iterations done:
     } else {
@@ -405,7 +407,7 @@ void MeasureIFPower::exitAction(bool success) {
         setStatusMessage(false, msg);
     }
     // Send the measurement finished event:
-    setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
+    setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, coldCart_m.getBand(), -1, 0, 100);
 }
 
 void MeasureIFPower::readData(bool printHeader) {

FrontEndAMBLib/src/NICANBusInterface.cpp
@@ -166,7 +166,7 @@ void NICANBusInterface::monitorImpl(unsigned long _handle, AmbMessage_t &msg)
                 if (monitorTimes.size() == N) {
                     calc_response_time.calculate(monitorTimes);
                     LOG(LM_INFO) << "monitor elapsed N=" << N << ", mean=" << calc_response_time.mean_m 
-                                 << ", stdev=" << calc_response_time.std_m << ", max=" << calc_response_time.max_m << " ms" << std::endl;
+                                 << ", stdev=" << calc_response_time.std_m << ", max=" << calc_response_time.max_m << " us" << std::endl;
                     monitorTimes.clear();
                 }
             }

FrontEndControl2/FEMCEventQueue.h
@@ -28,7 +28,8 @@ public:
         EVENT_OPTIMIZE_DONE     = 10,   // indicates that an optimization-related measurement is complete.  caller can collect the data.
         EVENT_CHOPPER_MOVE      = 11,   // asks the caller to move the chopper to hot or cold position.
         EVENT_INITIALIZE        = 12,   // sent after successful connection and querying of FE state.
-        EVENT_CARTHC_DONE       = 13    // sent when all cart health check measurements have finished.
+        EVENT_CARTHC_DONE       = 13,   // sent when all cart health check measurements have finished.
+        EVENT_ALL_REPS_DONE     = 14    // sent when all requested IVCURVE or OPTIMIZE repetitions are complete.
     };
 
     struct Event {

FrontEndControl2/FrontEndImpl.cpp
@@ -863,6 +863,12 @@ bool FrontEndImpl::setYIGLimits(int port, double FLOYIG, double FHIYIG) {
 bool FrontEndImpl::setCartridgeOff(int port) {
     LOG(LM_INFO) << "FrontEndImpl::setCartridgeOff port=" << port << endl;
 
+    // pause monitoring:
+    carts_mp -> pauseMonitor(port, true, true);
+
+    // delay to allow monitoring pause to take effect:
+    SLEEP(500);
+
     // set the CartAssembly to powered off state:
     carts_mp -> clearEnable(port);
 

FrontEndControl2/OPTIMIZE/MeasureFineLOSweep.cpp
@@ -193,6 +193,7 @@ void MeasureFineLOSweep::optimizeAction() {
             else
                 saveData(1);
         }
+        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, cartAssembly_m.getBand(), -1, 0, 100);
     }        
     if (!error) {
         string msg("MeasureFineLOSweep: finished successfully.");
@@ -231,7 +232,7 @@ void MeasureFineLOSweep::exitAction(bool success) {
     }
 
     // Send the measurement finished event:
-    setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, cartAssembly_m.getBand(), -1, 0, 100);
+    setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, cartAssembly_m.getBand(), -1, 0, 100);
 }
 
 void MeasureFineLOSweep::saveData(int pol) {

FrontEndControl2/OPTIMIZE/MeasureSISCurrent.cpp
@@ -209,6 +209,7 @@ void MeasureSISCurrent::optimizeAction() {
             progress_m += progressIncrement;
             setProgress((short) progress_m);
         }
+        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
     }
 
     // sweep the SIS magnet(s) current down to 0:
@@ -239,7 +240,7 @@ void MeasureSISCurrent::optimizeAction() {
         dataFile_mp = NULL;
     }
     setProgress(100);
-    setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
+    setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, coldCart_m.getBand(), -1, 0, 100);
     if (stopRequested()) {
         string msg("MeasureSISCurrent: process stopped.");
         setStatusMessage(false, msg);

FrontEndControl2/OPTIMIZE/MixerDeflux.cpp
@@ -275,13 +275,13 @@ void MixerDeflux::optimizeAction() {
 
     // Send the measurement finished event:
     setProgress(100);
+    setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, cca_m -> getBand(), -1, 0, 100);
+    setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, cca_m -> getBand(), -1, 0, 100);
     if (stopRequested()) {
-        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, cca_m -> getBand(), -1, 0, 100);
         string msg("MixerDeflux: process stopped.");
         setStatusMessage(false, msg);
         setFinished(false);
     } else {
-        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, cca_m -> getBand(), -1, 0, 100);
         string msg("MixerDeflux: finished successfully.");
         setStatusMessage(true, msg);
         setFinished(true);

FrontEndControl2/makefile
@@ -100,7 +100,7 @@ FRONTENDOUT = $(patsubst %.cpp,%.o,$(wildcard $(FRONTENDCPP)))
 frontend: $(FRONTENDOUT)
 $(FRONTENDOUT): $(FRONTENDCPP)
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) \
-	$(PROJECTINC) $(PORTABLEINC) $(MYSQLINC) \
+	$(PROJECTINC) $(MYSQLINC) \
 	-c $(FRONTENDCPP)
 
 # List of all output files:
@@ -115,8 +115,8 @@ dll: FrontEndControl.dll DLL/libFrontEndControl.a DLL/FrontEndControl.def
 FrontEndControl.dll DLL/FrontEndControl.def: $(DLLCPP) $(ALLOUT)
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) -DBUILD_FRONTENDCONTROL -shared -o FrontEndControl.dll \
 	$(ALLOUT) \
-	$(PROJECTINC) $(PORTABLEINC) $(MYSQLINC) \
-	$(DBLIB) $(AMBLIB) $(UTILLIB) $(MYSQLLIB) $(WINLIB) $(NICANLIB) $(PORTABLELIB) $(EXCHNDLIB) $(EXPATLIB) $(EXPATMMLIB) \
+	$(PROJECTINC) $(MYSQLINC) \
+	$(DBLIB) $(AMBLIB) $(UTILLIB) $(MYSQLLIB) $(WINLIB) $(NICANLIB) $(EXCHNDLIB) $(EXPATLIB) $(EXPATMMLIB) \
 	-Wl,--enable-auto-import,--output-def,DLL/FrontEndControl.def
 
 # Build the import library for linking to the DLL:
@@ -148,26 +148,24 @@ t_SocketClient.exe : tests/t_SocketClient.cpp
 t_LookupTables.exe : tests/t_LookupTables.cpp
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) -o t_LookupTables.exe \
 	tests/t_LookupTables.cpp CONFIG/LookupTables.o \
-	$(PORTABLEINC) \
-	$(PORTABLELIB)
+	$(PROJECTINC)
 
 t_semaphore_leaks.exe : tests/t_semaphore_leaks.cpp
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) -o t_semaphore_leaks.exe \
 	tests/t_semaphore_leaks.cpp \
-	$(PORTABLEINC) \
-	$(PORTABLELIB)
+	$(PROJECTINC)
 
 t_StreamLogger.exe : tests/t_StreamLogger.cpp
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) -o t_StreamLogger.exe \
 	tests/t_StreamLogger.cpp $(AMBOUT) $(BASICOUT) \
 	$(PROJECTINC) \
-	$(UTILLIB) $(PORTABLELIB) $(AMBLIB)
+	$(UTILLIB) $(AMBLIB)
 
 t_FEICDataBase.exe : tests/t_FEICDataBase.cpp CONFIG/FrontEndDatabase.cpp
 	g++ $(CPPFLAGS) $(DEBUGFLAGS) -o t_FEICDataBase.exe \
 	tests/t_FEICDataBase.cpp CONFIG/FrontEndDatabase.cpp CONFIG/LookupTables.cpp DLL/SWVersion.cpp \
 	$(PROJECTINC) $(MYSQLINC) \
-	$(DBLIB) $(UTILLIB) $(MYSQLLIB) $(PORTABLELIB)
+	$(DBLIB) $(UTILLIB) $(MYSQLLIB)
 
 .PHONY: all
 all: allout dll tests

commit 1e78b72919ecec282ba82792f9c7cce4edef28c7

2.17.2 2025-03-25   Fix bug introduced in commit dbe2c97de0: was sending OPTIMIZE_DONE event in the measurement loop for IF power

ALMA-FEControl.code-workspace
@@ -1,0 +1,86 @@
+{
+	"folders": [
+		{
+			"path": "."
+		}
+	],
+	"settings": {
+		"window.title": "${dirty}${activeEditorShort}${separator}${folderPath}${separator}${appName}",
+		"files.associations": {
+			"cctype": "cpp",
+			"clocale": "cpp",
+			"cmath": "cpp",
+			"csignal": "cpp",
+			"cstdarg": "cpp",
+			"cstddef": "cpp",
+			"cstdio": "cpp",
+			"cstdlib": "cpp",
+			"cstring": "cpp",
+			"ctime": "cpp",
+			"cwchar": "cpp",
+			"cwctype": "cpp",
+			"any": "cpp",
+			"array": "cpp",
+			"atomic": "cpp",
+			"strstream": "cpp",
+			"bit": "cpp",
+			"*.tcc": "cpp",
+			"charconv": "cpp",
+			"chrono": "cpp",
+			"codecvt": "cpp",
+			"compare": "cpp",
+			"concepts": "cpp",
+			"condition_variable": "cpp",
+			"coroutine": "cpp",
+			"cstdint": "cpp",
+			"deque": "cpp",
+			"list": "cpp",
+			"map": "cpp",
+			"set": "cpp",
+			"string": "cpp",
+			"unordered_map": "cpp",
+			"vector": "cpp",
+			"exception": "cpp",
+			"algorithm": "cpp",
+			"functional": "cpp",
+			"iterator": "cpp",
+			"memory": "cpp",
+			"memory_resource": "cpp",
+			"numeric": "cpp",
+			"optional": "cpp",
+			"random": "cpp",
+			"ratio": "cpp",
+			"source_location": "cpp",
+			"string_view": "cpp",
+			"system_error": "cpp",
+			"tuple": "cpp",
+			"type_traits": "cpp",
+			"utility": "cpp",
+			"format": "cpp",
+			"fstream": "cpp",
+			"future": "cpp",
+			"initializer_list": "cpp",
+			"iomanip": "cpp",
+			"iosfwd": "cpp",
+			"iostream": "cpp",
+			"istream": "cpp",
+			"limits": "cpp",
+			"mutex": "cpp",
+			"new": "cpp",
+			"numbers": "cpp",
+			"ostream": "cpp",
+			"semaphore": "cpp",
+			"shared_mutex": "cpp",
+			"span": "cpp",
+			"sstream": "cpp",
+			"stdexcept": "cpp",
+			"stop_token": "cpp",
+			"streambuf": "cpp",
+			"thread": "cpp",
+			"cinttypes": "cpp",
+			"typeinfo": "cpp",
+			"variant": "cpp",
+			"*.ipp": "cpp"
+		}
+	}
+}

FrontEndControl2/DLL/SWVersion.cpp
@@ -21,14 +21,17 @@
 
 #include "SWVersion.h"
 
-const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.17.1");
+const std::string FECONTROL_SW_VERSION_STRING("FEControl 2.17.2");
 
 //******* Be sure to update resource.rc!
 
 
 /* Revision history:
 
-2.17.1
+2.17.2 2025-03-25
+  Fix bug introduced in commit dbe2c97de0: was sending OPTIMIZE_DONE event in the measurement loop for IF power
+
+2.17.1 2025-02-26
   Fix Fine LO Sweep data being inserted with DataStatus 1=PAS, should be 3=PAI
 
 2.17.0

FrontEndControl2/DLL/resource.rc
@@ -2,8 +2,8 @@
 
 // DLL version information.
 VS_VERSION_INFO    VERSIONINFO
-FILEVERSION        2,17,1,0
-PRODUCTVERSION     2,17,1,0
+FILEVERSION        2,17,2,0
+PRODUCTVERSION     2,17,2,0
 FILEFLAGSMASK      VS_FFI_FILEFLAGSMASK
 #ifdef _DEBUG
   FILEFLAGS        VS_FF_DEBUG | VS_FF_PRERELEASE
@@ -20,12 +20,12 @@ BEGIN
     BEGIN
       VALUE "CompanyName", "NRAO"
       VALUE "FileDescription", "Front End Control DLL"
-      VALUE "FileVersion", "2.17.1 NRAO-B2"
+      VALUE "FileVersion", "2.17.2"
       VALUE "InternalName", "FEControl"
       VALUE "LegalCopyright", "2025 NRAO"
       VALUE "OriginalFilename", "FrontEndControl.dll"
       VALUE "ProductName", "Front End Control DLL"
-      VALUE "ProductVersion", "2.17.1 NRAO-B2"
+      VALUE "ProductVersion", "2.17.2"
     END
   END
   BLOCK "VarFileInfo"

FrontEndControl2/OPTIMIZE/MeasureIFPower.cpp
@@ -362,13 +362,12 @@ void MeasureIFPower::optimizeAction() {
             SLEEP(stepSleep);
             waitForPowerReading_m = true;
         }
-        // Send the measurement finished event:
-        setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
-        
-    // All iterations done:
+        // All iterations done:
     } else {
         waitForPowerReading_m = false;
         setFinished(true);
+        // Send the all reps done event.
+        setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, coldCart_m.getBand(), -1, 0, 100);
     }
 }
 
@@ -407,7 +406,7 @@ void MeasureIFPower::exitAction(bool success) {
         setStatusMessage(false, msg);
     }
     // Send the measurement finished event:
-    setEvent(FEMCEventQueue::EVENT_ALL_REPS_DONE, coldCart_m.getBand(), -1, 0, 100);
+    setEvent(FEMCEventQueue::EVENT_OPTIMIZE_DONE, coldCart_m.getBand(), -1, 0, 100);
 }
 
 void MeasureIFPower::readData(bool printHeader) {